3d43752321d44a9e70b24abb073172a165d4e0f2
Charlie Arboleda <charliearboleda@Charlies-Air.lan>
2021-09-22 00:03:15 -0400

    included checkout page update cart remove cart items integrated stripe into the build, completed styling and tested all functions are working as intended

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,36 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
+import { ShoppingCart } from '@material-ui/icons';
+import { Link, useLocation } from 'react-router-dom';
 
-import { AppBar, Toolbar, IconButton, Badge, Menu, MenuItem, Typography } from '@material-ui/core'
+import logo from '../../assets/online-store.png';
+import useStyles from './styles';
 
-import { ShoppingCart } from '@material-ui/icons'
-import logo from '../../assets/online-store.png'
+const PrimarySearchAppBar = ({ totalItems }) => {
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = useState(null);
+  const classes = useStyles();
+  const location = useLocation();
 
-import useStyles from './styles'
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
 
-const Navbar = ({ totalItems }) => {
-    const classes = useStyles()
+  const handleMobileMenuClose = () => setMobileMoreAnchorEl(null);
 
-    return (
-        <div>
-            <AppBar position='fixed' className={classes.appBar} color='inherit'>
-                <Toolbar>
-                    <Typography variant='h6' className={classes.title} color='inherit'>
-                        <img src={logo} alt='WebifiedShopping' height='25px' className={classes.image} />
-                        WebifiedShopping
-                    </Typography>
-                    <div className={classes.grow} />
-                    <div className={classes.button} >
-                        <IconButton aria-label='show cart times' color='inherit'>
-                            <Badge badgeContent={totalItems} color='secondary'>
-                                <ShoppingCart />
-                            </Badge>
-                        </IconButton>
+  const mobileMenuId = 'primary-search-account-menu-mobile';
 
-                    </div>
-                </Toolbar>
+  const renderMobileMenu = (
+    <Menu anchorEl={mobileMoreAnchorEl} anchorOrigin={{ vertical: 'top', horizontal: 'right' }} id={mobileMenuId} keepMounted transformOrigin={{ vertical: 'top', horizontal: 'right' }} open={isMobileMenuOpen} onClose={handleMobileMenuClose}>
+      <MenuItem>
+        <IconButton component={Link} to="/cart" aria-label="Show cart items" color="inherit">
+          <Badge badgeContent={totalItems} color="secondary">
+            <ShoppingCart />
+          </Badge>
+        </IconButton>
+        <p>Cart</p>
+      </MenuItem>
+    </Menu>
+  );
 
-            </AppBar>
-        </div>
-    )
-}
-export default Navbar
+  return (
+    <>
+      <AppBar position="fixed" className={classes.appBar} color="inherit">
+        <Toolbar>
+          <Typography component={Link} to="/" variant="h6" className={classes.title} color="inherit">
+            <img src={logo} alt="commerce.js" height="25px" className={classes.image} /> Commerce.js
+          </Typography>
+          <div className={classes.grow} />
+          {location.pathname === '/' && (
+          <div className={classes.button}>
+            <IconButton component={Link} to="/cart" aria-label="Show cart items" color="inherit">
+              <Badge badgeContent={totalItems} color="secondary">
+                <ShoppingCart />
+              </Badge>
+            </IconButton>
+          </div>
+          )}
+        </Toolbar>
+      </AppBar>
+      {renderMobileMenu}
+    </>
+  );
+};
+
+export default PrimarySearchAppBar
